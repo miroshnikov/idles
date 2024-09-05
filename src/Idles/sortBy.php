e447c9eb5b0f96f3dc87e8ff06e959667afd4181
Max Miroshnikov <miroshnikov@gmail.com>
2024-09-05 14:00:11 +0300

    fix #7: sortBy crashes when array is empty

--- src/Idles/sortBy.php
+++ src/Idles/sortBy.php
@@ -5,6 +5,9 @@ namespace Idles;
 function _sortBy(?iterable $collection, callable ...$iteratees): array
 {
     $collection = collect($collection);
+    if (empty($collection)) {
+        return [];
+    }
     $indexes = \range(0, \count($collection)-1);
     $arrays = concat(map(fn ($iteratee) => map($iteratee, $collection), $iteratees), [$indexes]);
     \array_multisort(...$arrays);
